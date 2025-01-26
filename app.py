946a8f90bd1d9c04ebce887a2c0c712b13ad3b3d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-26 21:01:42 +0900

    GPT-Challenge: AssistantGPT(API Key validation)

--- app.py
+++ app.py
@@ -11,57 +11,6 @@ from typing_extensions import override
 from openai import AssistantEventHandler
 
 
-class EventHandler(AssistantEventHandler):
-    message = ""
-
-    @override
-    def on_text_created(self, text) -> None:
-        self.message_box = st.empty()
-
-    def on_text_delta(self, delta, snapshot):
-        self.message += delta.value
-        self.message_box.markdown(self.message.replace("$", "\$"))
-
-    @override
-    def on_event(self, event):
-        # Retrieve events that are denoted with 'requires_action'
-        # since these will have our tool_calls
-        if event.event == "thread.run.requires_action":
-            run_id = event.data.id  # Retrieve the run ID from the event data
-            self.handle_requires_action(event.data, run_id)
-
-    def handle_requires_action(self, data, run_id):
-        tool_outputs = []
-
-        for tool in data.required_action.submit_tool_outputs.tool_calls:
-            tool_id = tool.id
-            function = tool.function
-            print(f"Calling function: {function.name} with arg {function.arguments}")
-            tool_outputs.append(
-                {
-                    "output": functions_map[function.name](
-                        json.loads(function.arguments)
-                    ),
-                    "tool_call_id": tool_id,
-                }
-            )
-
-        # Submit all tool_outputs at the same time
-        self.submit_tool_outputs(tool_outputs, run_id)
-
-    def submit_tool_outputs(self, tool_outputs, run_id):
-        # Use the submit_tool_outputs_stream helper
-        with client.beta.threads.runs.submit_tool_outputs_stream(
-            thread_id=self.current_run.thread_id,
-            run_id=self.current_run.id,
-            tool_outputs=tool_outputs,
-            event_handler=EventHandler(),
-        ) as stream:
-            for text in stream.text_deltas:
-                print(text, end="", flush=True)
-            print()
-
-
 def duckduckgo_search(inputs):
     ddg = DuckDuckGoSearchResults()
     query = inputs["query"]
@@ -197,74 +146,10 @@ functions = [
     },
 ]
 
-client = OpenAI()
-
 assistant_name = "Research Assistant"
-
-if "assistant" not in st.session_state:
-    assistants = client.beta.assistants.list()
-    for assistant in assistants:
-        # OpenAI에 이미 만들어준 Research Assistant가 있는지 확인
-        if assistant.name == assistant_name:
-            assistant = client.beta.assistants.retrieve(assistant.id)
-            print("Bringed the assistant from my OpenAI")
-            break
-        else:
-            assistant = client.beta.assistants.create(
-                name="Research Assistant",
-                instructions="""
-                    You are a research expert.
-
-                    Your task is to use Wikipedia or DuckDuckGo to gather comprehensive and accurate information about the query provided.
-
-                    When you find a relevant website through DuckDuckGo, you must scrape the content from that website. Use this scraped content to thoroughly research and formulate a detailed answer to the question.
-
-                    Combine information from Wikipedia, DuckDuckGo searches, and any relevant websites you find. Ensure that the final answer is well-organized and detailed, and include citations with links (URLs) for all sources used.
-
-                    Your research should be saved to a .txt file, and the content should match the detailed findings provided. Make sure to include all sources and relevant information.
-
-                    The information from Wikipedia must be included.
-
-                    Ensure that the final .txt file contains detailed information, all relevant sources, and citations.
-                    """,
-                model="gpt-4o-mini",
-                tools=functions,
-            )
-            print("Created a assistant")
-            break
-    print("Assistant name: ", assistant.name)
-    print("Assistant ID: ", assistant.id)
-    st.session_state["assistant"] = assistant
-    print("Put the assistant in session state")
-    thread = client.beta.threads.create()
-    st.session_state["thread"] = thread
-    print("Put the thread in session state")
-
-else:
-    assistant = st.session_state["assistant"]
-    print("Assistant name: ", assistant.name)
-    print("Assistant ID: ", assistant.id)
-    print("use assistant in session state")
-    thread = st.session_state["thread"]
-    print("use thread in session state")
 
 
 # Streamlit
-
-st.set_page_config(
-    page_title=f"{assistant_name}",
-    page_icon="🔍",
-)
-
-st.title(f"{assistant_name}")
-
-st.markdown(
-    """
-    This GPT uses DuckDuckGo and Wikipedia to research whatever you want to know. It summarizes the gathered information for you and provides a file containing the details from the referenced sources.
-    """
-)
-
-# Utility Functions
 
 
 # API 키 유효성 검사 함수
@@ -281,80 +166,190 @@ def validate_openai_api_key(api_key):
         return False
 
 
-# Thread 실행
-def get_run(run_id, thread_id):
-    return client.beta.threads.runs.retrieve(
-        run_id=run_id,
-        thread_id=thread_id,
-    )
+st.set_page_config(
+    page_title=f"{assistant_name}",
+    page_icon="🔍",
+)
 
+st.title(f"{assistant_name}")
 
-# Thread에 메시지 보내기
-def send_message(thread_id, content):
-    return client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=content,
-    )
-
-
-# Thread에 있는 메시지 받기
-def get_messages(thread_id):
-    messages = client.beta.threads.messages.list(thread_id=thread_id)
-    messages = list(messages)
-    messages.reverse()
-    return messages
-
-
-# Chat Message
-def show_chat(role, message):
-    with st.chat_message(role):
-        st.markdown(message)
-
-
-# Paint History
-def paint_history(thread_id):
-    messages = get_messages(thread_id)
-    for message in messages:
-        show_chat(
-            message.role,
-            message.content[0].text.value,
-        )
+st.markdown(
+    """
+    This GPT uses DuckDuckGo and Wikipedia to research whatever you want to know. It summarizes the gathered information for you and provides a file containing the details from the referenced sources.
+    """
+)
 
 
 def main():
+    class EventHandler(AssistantEventHandler):
+        message = ""
+
+        @override
+        def on_text_created(self, text) -> None:
+            self.message_box = st.empty()
+
+        def on_text_delta(self, delta, snapshot):
+            self.message += delta.value
+            self.message_box.markdown(self.message.replace("$", "\$"))
+
+        @override
+        def on_event(self, event):
+            # Retrieve events that are denoted with 'requires_action'
+            # since these will have our tool_calls
+            if event.event == "thread.run.requires_action":
+                run_id = event.data.id  # Retrieve the run ID from the event data
+                self.handle_requires_action(event.data, run_id)
+
+        def handle_requires_action(self, data, run_id):
+            tool_outputs = []
+
+            for tool in data.required_action.submit_tool_outputs.tool_calls:
+                tool_id = tool.id
+                function = tool.function
+                print(
+                    f"Calling function: {function.name} with arg {function.arguments}"
+                )
+                tool_outputs.append(
+                    {
+                        "output": functions_map[function.name](
+                            json.loads(function.arguments)
+                        ),
+                        "tool_call_id": tool_id,
+                    }
+                )
+
+            # Submit all tool_outputs at the same time
+            self.submit_tool_outputs(tool_outputs, run_id)
+
+        def submit_tool_outputs(self, tool_outputs, run_id):
+            # Use the submit_tool_outputs_stream helper
+            with client.beta.threads.runs.submit_tool_outputs_stream(
+                thread_id=self.current_run.thread_id,
+                run_id=self.current_run.id,
+                tool_outputs=tool_outputs,
+                event_handler=EventHandler(),
+            ) as stream:
+                for text in stream.text_deltas:
+                    print(text, end="", flush=True)
+                print()
+
+    # Utility Functions
+
+    # Thread 실행
+    def get_run(run_id, thread_id):
+        return client.beta.threads.runs.retrieve(
+            run_id=run_id,
+            thread_id=thread_id,
+        )
+
+    # Thread에 메시지 보내기
+    def send_message(thread_id, content):
+        return client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=content,
+        )
+
+    # Thread에 있는 메시지 받기
+    def get_messages(thread_id):
+        messages = client.beta.threads.messages.list(thread_id=thread_id)
+        messages = list(messages)
+        messages.reverse()
+        return messages
+
+    # Chat Message
+    def show_chat(role, message):
+        with st.chat_message(role):
+            st.markdown(message)
+
+    # Paint History
+    def paint_history(thread_id):
+        messages = get_messages(thread_id)
+        for message in messages:
+            show_chat(
+                message.role,
+                message.content[0].text.value,
+            )
 
     if openai_api_key:
         st.write(
             "##### Step 2. Enter the topic you want to research in the input field below"
         )
+        client = OpenAI()
+
+        if "assistant" not in st.session_state:
+            assistants = client.beta.assistants.list()
+            for assistant in assistants:
+                # OpenAI에 이미 만들어준 Research Assistant가 있는지 확인
+                if assistant.name == assistant_name:
+                    assistant = client.beta.assistants.retrieve(assistant.id)
+                    print("Bringed the assistant from my OpenAI")
+                    break
+                else:
+                    assistant = client.beta.assistants.create(
+                        name="Research Assistant",
+                        instructions="""
+                            You are a research expert.
+
+                            Your task is to use Wikipedia or DuckDuckGo to gather comprehensive and accurate information about the query provided.
+
+                            When you find a relevant website through DuckDuckGo, you must scrape the content from that website. Use this scraped content to thoroughly research and formulate a detailed answer to the question.
+
+                            Combine information from Wikipedia, DuckDuckGo searches, and any relevant websites you find. Ensure that the final answer is well-organized and detailed, and include citations with links (URLs) for all sources used.
+
+                            Your research should be saved to a .txt file, and the content should match the detailed findings provided. Make sure to include all sources and relevant information.
+
+                            The information from Wikipedia must be included.
+
+                            Ensure that the final .txt file contains detailed information, all relevant sources, and citations.
+                            """,
+                        model="gpt-4o-mini",
+                        tools=functions,
+                    )
+                    print("Created a assistant")
+                    break
+            print("Assistant name: ", assistant.name)
+            print("Assistant ID: ", assistant.id)
+            st.session_state["assistant"] = assistant
+            print("Put the assistant in session state")
+            thread = client.beta.threads.create()
+            st.session_state["thread"] = thread
+            print("Put the thread in session state")
+
+        else:
+            assistant = st.session_state["assistant"]
+            print("Assistant name: ", assistant.name)
+            print("Assistant ID: ", assistant.id)
+            print("use assistant in session state")
+            thread = st.session_state["thread"]
+            print("use thread in session state")
+
+        paint_history(thread.id)
+
+        query = st.chat_input("Write down the topic you want to research")
+
+        if query:
+            send_message(thread.id, query)
+            show_chat("user", query)
+
+            with st.chat_message("assistant"):
+                with st.spinner("In progress..."):
+                    with client.beta.threads.runs.stream(
+                        thread_id=thread.id,
+                        assistant_id=assistant.id,
+                        event_handler=EventHandler(),
+                    ) as stream:
+                        stream.until_done()
+                    file_path = f"research_results_{query}.txt"
+                    with open(file_path, "rb") as f:
+                        st.download_button(
+                            f"Download {file_path}",
+                            f,
+                            file_name=f"{file_path}",
+                            mime="text/plain",
+                        )
     else:
         st.write("##### Step 1. Add your OpenAI API Key")
-
-    paint_history(thread.id)
-
-    query = st.chat_input("Write down the topic you want to research")
-
-    if query:
-        send_message(thread.id, query)
-        show_chat("user", query)
-
-        with st.chat_message("assistant"):
-            with st.spinner("In progress..."):
-                with client.beta.threads.runs.stream(
-                    thread_id=thread.id,
-                    assistant_id=assistant.id,
-                    event_handler=EventHandler(),
-                ) as stream:
-                    stream.until_done()
-                file_path = f"research_results_{query}.txt"
-                with open(file_path, "rb") as f:
-                    st.download_button(
-                        f"Download {file_path}",
-                        f,
-                        file_name=f"{file_path}",
-                        mime="text/plain",
-                    )
 
 
 with st.sidebar:
